1da083522af489af4a5baf7bf908852649ae88d7
Megdela <megdela21@gmail.com>
2025-05-07 03:21:20 +0300

    remove preview

--- app/src/main/java/com/jct/renthabesha/core/presentation/ui/screens/SplashScreen.kt
+++ app/src/main/java/com/jct/renthabesha/core/presentation/ui/screens/SplashScreen.kt
@@ -21,7 +21,6 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.jct.renthabesha.R
@@ -44,7 +43,7 @@ import com.jct.renthabesha.R
 //        }
 //    }
 //}
-@Preview
+
 @Composable
 fun SplashScreen() {
     Box(
--- app/src/main/java/com/jct/renthabesha/core/presentation/ui/screens/WelcomeScreen.kt
+++ app/src/main/java/com/jct/renthabesha/core/presentation/ui/screens/WelcomeScreen.kt
@@ -21,11 +21,10 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.jct.renthabesha.R
-@Preview
+
 @Composable
 fun WelcomeScreen(onClick: () -> Unit = {}) {
     Column(
